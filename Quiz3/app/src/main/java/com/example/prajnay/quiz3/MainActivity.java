2b1b332e2315dbca119fce2a84f89cc91e3d351a
Prajnay Kataria <34885815+prajnay@users.noreply.github.com>
2018-12-16 15:56:07 +0530

    Add files via upload

---
+++ Quiz3/app/src/main/java/com/example/prajnay/quiz3/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.prajnay.quiz3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.RadioButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Scorecal(View view) {
+        Check_Q1();
+        Check_Q2();
+        Check_Q3();
+        Check_Q4();
+        finalscore(score);
+        Reset();
+    }
+
+    private void Check_Q1() {
+        String ans = getString(R.string.A1);
+        EditText userans = (EditText) findViewById(R.id.userans1);
+        Editable user_ans = userans.getText();
+        String ansbyuser = user_ans.toString();
+        if (ansbyuser.equals(ans)) {
+            increment();
+        }
+        return;
+    }
+
+    private void Check_Q2() {
+        CheckBox op1 = findViewById(R.id.a2op1);
+        CheckBox op2 = findViewById(R.id.a2op2);
+        CheckBox op3 = findViewById(R.id.a2op3);
+        boolean checkop1=op1.isChecked();
+        boolean checkop2=op2.isChecked();
+        boolean checkop3=op3.isChecked();
+        if (checkop2 && checkop1){
+            if(checkop3){
+                score=score-1;
+            }else {
+                increment();
+            }
+        }else {
+            if(checkop3){
+                score=score-1;
+            }
+        }
+    }
+
+    private void Check_Q3() {
+        RadioButton True1 = findViewById(R.id.Q3T);
+        if (True1.isChecked()) {
+            increment();
+        }
+    }
+
+    private void Check_Q4() {
+        EditText userans1 = (EditText) findViewById(R.id.userans2);
+        Editable user_ans1 = userans1.getText();
+        String ansbyuser1 = user_ans1.toString();
+        String ans1 = getString(R.string.A4);
+        if (ansbyuser1.equals(ans1)) {
+            increment();
+        }
+    }
+
+    int score = 0;
+
+    private void increment() {
+        score = score + 1;
+    }
+
+    private void finalscore(int fscore) {
+        TextView score1 = (TextView) findViewById(R.id.Score);
+        score1.setText("Your score is " + fscore);
+    }
+
+    private void Reset() {
+        score = 0;
+    }
+}
+
+
+
+
+
+
+
+
